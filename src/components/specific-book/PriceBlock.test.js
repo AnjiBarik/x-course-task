1f1249725f65b9faadab9a0cd256a14ca3f49beb
AnjiBarik <cssjavascr@gmail.com>
2023-08-07 23:07:55 +0300

    besst

--- src/components/specific-book/PriceBlock.test.js
+++ src/components/specific-book/PriceBlock.test.js
@@ -1,21 +1,34 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, screen } from '@testing-library/react';
 import PriceBlock from './PriceBlock';
 
-test('increment button increases count by 1', () => {
-  const { getByTestId } = render(<PriceBlock title="Book Title" price={10} cartItems={[]} theme="light" />);
-  const incrementButton = getByTestId('increment-btn');
-  const countLabel = getByTestId('count-label');
+test('increment button increases count by 1 and changes total price', () => {
+  render(<PriceBlock title="Book Title" price={10} cartItems={[]} theme="light" />);
+  
+  const incrementButton = screen.getByTestId('increment-btn');
+  const countLabel = screen.getByTestId('count-label');
+  const totalPriceElement = screen.getByTestId('totalPrice');
 
+  
+  expect(countLabel.textContent).toBe('1');
+  expect(totalPriceElement.textContent).toBe('$10.00');
+
+  
   fireEvent.click(incrementButton);
+   
   expect(countLabel.textContent).toBe('2');
+  expect(totalPriceElement.textContent).toBe('$20.00');
 });
 
 test('input value changes total price', () => {
-  const { getByTestId } = render(<PriceBlock title="Book Title" price={10} cartItems={[]} theme="light" />);
-  const inputElement = getByTestId('count-input');
-  const totalPriceElement = getByTestId('totalPrice');
+  render(<PriceBlock title="Book Title" price={10} cartItems={[]} theme="light" />);
+  
+  const inputElement = screen.getByTestId('count-input');
+  const totalPriceElement = screen.getByTestId('totalPrice');
+
+  expect(totalPriceElement.textContent).toBe('$10.00');
 
   fireEvent.change(inputElement, { target: { value: '5' } });
+
   expect(totalPriceElement.textContent).toBe('$50.00');
 });
